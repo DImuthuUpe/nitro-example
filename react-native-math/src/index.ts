426799d43c691a5c2b31cb8fc8c16bc98649341d
DImuthuUpe <dimuthu.upeksha2@gmail.com>
2025-01-30 17:07:09 -0500

    Adding example project

--- react-native-math/src/index.ts
+++ react-native-math/src/index.ts
@@ -1,1 +1,5 @@
+import { NitroModules } from "react-native-nitro-modules";
+import type { Math } from "./specs/Example.nitro";
+
 // TODO: Export all HybridObjects here for the user
+export const MathModule = NitroModules.createHybridObject<Math>("Math")
--- react-native-math/src/specs/Example.nitro.ts
+++ react-native-math/src/specs/Example.nitro.ts
@@ -1,1 +1,5 @@
-// TODO: Export specs that extend HybridObject<...> here
+import { type HybridObject } from 'react-native-nitro-modules'
+
+export interface Math extends HybridObject<{ ios: 'c++', android: 'c++' }> {
+    add(a: number, b: number): number
+}
